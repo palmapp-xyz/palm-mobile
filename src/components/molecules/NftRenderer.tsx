900d89d9a8ddcdcce8bfa4b4e93a4f7599f43420
Jong Hyuck Won <jhwon0820@gmail.com>
2023-02-27 15:22:19 +0900

    nft renderer fix

--- src/components/molecules/NftRenderer.tsx
+++ src/components/molecules/NftRenderer.tsx
@@ -1,41 +1,50 @@
 import React, { ReactElement } from 'react'
-import { Image } from 'react-native'
-import { SvgUri } from 'react-native-svg'
 
 import { ContractAddr, QueryKeyEnum } from 'types'
 import useReactQuery from 'hooks/complex/useReactQuery'
 import useNft from 'hooks/contract/useNft'
-import { nftUriFetcher } from 'libs/nft'
+import { fetchNftImage } from 'libs/fetchTokenUri'
+import ErrorBoundary from 'components/atoms/ErrorBoundary'
+import FallbackMediaRenderer from './FallbackMediaRenderer'
+import { MediaRenderer } from './MediaRenderer'
+import { FlexStyle } from 'react-native'
 
 const NftRenderer = ({
   nftContract,
   tokenId,
+  width,
+  height,
 }: {
   nftContract: ContractAddr
   tokenId: string
+  width?: FlexStyle['width']
+  height?: FlexStyle['height']
 }): ReactElement => {
   const { tokenURI } = useNft({ nftContract })
 
-  const { data: { uri, type } = { uri: '', type: '' } } = useReactQuery(
+  const { data: uri = '' } = useReactQuery(
     [QueryKeyEnum.NFT_TOKEN_URI, nftContract, tokenId],
     async () => {
       const tokenUri = await tokenURI({ tokenId })
-
       if (tokenUri) {
-        return nftUriFetcher(tokenUri)
+        return fetchNftImage({ tokenUri })
       }
     }
   )
 
+  const props = {
+    src: typeof uri === 'string' ? uri : `${uri}`,
+    alt: `${nftContract}:${tokenId}`,
+    width: width || '100%',
+    height: height || '100%',
+  }
+
+  const fallback = <FallbackMediaRenderer {...props} />
+
   return (
-    <>
-      {type.includes('image') &&
-        (type.includes('svg') ? (
-          <SvgUri width="100%" height="100%" uri={uri} />
-        ) : (
-          <Image source={{ uri }} style={{ width: '100%', height: '100%' }} />
-        ))}
-    </>
+    <ErrorBoundary fallback={fallback}>
+      <MediaRenderer {...props} />
+    </ErrorBoundary>
   )
 }
 
