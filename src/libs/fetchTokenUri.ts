b352a1d509f615a4996c86dade79c223aad754ed
Jong Hyuck Won <jhwon0820@gmail.com>
2023-02-27 11:11:14 +0900

    fix nft renderering in goerli

--- src/libs/fetchTokenUri.ts
+++ src/libs/fetchTokenUri.ts
@@ -5,6 +5,8 @@ import { unescape } from './utils'
 export const fixURL = (uri: string): string => {
   if (uri.startsWith('https://ipfs.moralis.io:2053/ipfs/')) {
     uri = uri.replace('https://ipfs.moralis.io:2053/ipfs/', 'ipfs://')
+  } else if (uri.startsWith('https://ipfs.io/ipfs/')) {
+    uri = uri.replace('https://ipfs.io/ipfs/', 'ipfs://')
   } else if (uri.match(/^[a-zA-Z0-9_]+$/)) {
     // uri is just ipfs cid
     uri = `ipfs://${uri}`
@@ -28,20 +30,26 @@ export const fetchNftImage = async ({
     const ret =
       metadataJson?.image || metadataJson?.image_url || metadataJson?.image_data
     if (ret) {
-      return unescape(ret)
+      return unescape(fixURL(ret))
     }
   }
 
   if (tokenUri) {
     try {
-      const fetched = await fetch(fixURL(tokenUri))
+      const fixedUrl = fixURL(tokenUri)
+      const fetched = await fetch(fixedUrl)
+      const blob = await fetched.blob()
+      if (blob.type.startsWith('image')) {
+        return fixedUrl
+      }
+
       const jsonData = await fetched.json()
       const ret = jsonData?.image || jsonData?.image_url || jsonData?.image_data
       if (ret) {
         return unescape(ret)
       }
     } catch (e) {
-      console.error('fetchTokenUri failed: ', tokenUri, e)
+      console.error('fetchTokenUri failed: ', metadata, tokenUri, e)
     }
   }
 
