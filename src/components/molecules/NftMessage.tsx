a248aebb313afe7ad7c0f00cfe2de0183c0d1618
Jong Hyuck Won <jhwon0820@gmail.com>
2023-02-28 16:20:22 +0900

    nft message component styling

--- src/components/molecules/NftMessage.tsx
+++ src/components/molecules/NftMessage.tsx
@@ -1,20 +1,72 @@
-import React, { ReactElement } from 'react'
-
 import FileMessage, {
   FileMessageProps,
 } from '@sendbird/uikit-react-native/src/components/MessageRenderer/FileMessage'
 import { MediaRenderer } from './MediaRenderer'
 
+import React, { ReactElement } from 'react'
+import { Text, View } from 'react-native'
+
+import {
+  createStyleSheet,
+  useUIKitTheme,
+} from '@sendbird/uikit-react-native-foundation'
 const NftMessage = (props: FileMessageProps): ReactElement => {
-  const fileType = props.message.customType
-  if (fileType === 'share') {
-    return <MediaRenderer src={props.message.data} width={200} height={200} />
-  } else if (fileType === 'sell') {
-    return <MediaRenderer src={props.message.data} width={300} height={300} />
-  } else if (fileType === 'send') {
-    return <MediaRenderer src={props.message.data} width={400} height={400} />
+  const { colors } = useUIKitTheme()
+  const { message, children, variant } = props
+  if (!message.customType) {
+    return <FileMessage {...props} />
   }
-  return <FileMessage {...props} />
+
+  return (
+    <View
+      style={[
+        styles.bubbleContainer,
+        {
+          backgroundColor: colors.ui.message[variant].enabled.background,
+        },
+      ]}>
+      <View
+        style={[
+          styles.image,
+          {
+            backgroundColor: colors.onBackground04,
+          },
+        ]}>
+        <Text
+          style={{ color: colors.secondary, fontSize: 16, marginBottom: 12 }}>
+          {message.customType}
+        </Text>
+        <MediaRenderer
+          key={message.messageId}
+          src={message.data}
+          width={180}
+          height={180}
+          style={{ margin: 'auto' }}
+        />
+      </View>
+      {children}
+    </View>
+  )
 }
+
+const styles = createStyleSheet({
+  bubbleContainer: {
+    borderRadius: 16,
+    overflow: 'hidden',
+  },
+  image: {
+    width: 240,
+    maxWidth: 240,
+    height: 240,
+    borderRadius: 16,
+    overflow: 'hidden',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  hide: {
+    display: 'none',
+  },
+})
 
 export default React.memo(NftMessage)
