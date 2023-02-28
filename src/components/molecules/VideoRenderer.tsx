08c149bb25dad64d4541d7c2b9441d6124f1c379
Jong Hyuck Won <jhwon0820@gmail.com>
2023-02-28 09:50:23 +0900

    videorenderer thumbnail generation

--- src/components/molecules/VideoRenderer.tsx
+++ src/components/molecules/VideoRenderer.tsx
@@ -1,8 +1,10 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { View } from 'react-native'
-
 import Video from 'react-native-video'
+import { useAsyncEffect } from '@sendbird/uikit-utils'
 import { MediaRendererProps } from './MediaRenderer'
+import { usePlatformService } from '@sendbird/uikit-react-native'
+import SBUUtils from '@sendbird/uikit-react-native/src/libs/SBUUtils'
 
 const VideoRenderer = ({
   src,
@@ -11,9 +13,28 @@ const VideoRenderer = ({
   height,
   audioOnly,
 }: MediaRendererProps & { audioOnly?: boolean }): ReactElement => {
+  const [thumbnail, setThumbnail] = useState()
+  const { mediaService } = usePlatformService()
+
+  useAsyncEffect(async () => {
+    let thumb
+    if (src) {
+      await SBUUtils.safeRun(async () => {
+        const result = await mediaService.getVideoThumbnail({
+          url: src,
+          timeMills: 1000,
+        })
+        thumb = result?.path
+      })
+    }
+    setThumbnail(thumb)
+  }, [src])
+
   return (
     <View style={[{ position: 'relative', width, height }, style]}>
       <Video
+        posterResizeMode={'contain'}
+        poster={thumbnail}
         source={{ uri: src ?? undefined }}
         repeat={true}
         muted={true}
