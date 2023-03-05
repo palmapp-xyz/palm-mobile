85b7d76e04cd27c562a6e9964e8ffa1439a2c65c
Lim SangHyun <skqksh@gmail.com>
2023-03-04 19:17:05 -0700

    Update token gating screen

--- src/screens/ChannelTokenGatingScreen.tsx
+++ src/screens/ChannelTokenGatingScreen.tsx
@@ -1,19 +1,59 @@
 import React, { ReactElement, useMemo, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
+import _ from 'lodash'
+import { utils } from 'ethers'
 
 import { COLOR } from 'consts'
 
-import { Container, FormButton, FormInput, Header } from 'components'
-import { ContractAddr } from 'types'
+import {
+  Container,
+  FormButton,
+  FormImage,
+  FormInput,
+  Header,
+  MoralisNftRenderer,
+  Row,
+} from 'components'
+import { ContractAddr, QueryKeyEnum } from 'types'
 import { Routes } from 'libs/navigation'
 import { useAppNavigation } from 'hooks/useAppNavigation'
 import useFsChannel from 'hooks/firestore/useFsChannel'
-import { utils } from 'ethers'
+import useUserNftList from 'hooks/api/useUserNftList'
+import useAuth from 'hooks/independent/useAuth'
+import useNftImage from 'hooks/independent/useNftImage'
+import useReactQuery from 'hooks/complex/useReactQuery'
+import useNft from 'hooks/contract/useNft'
+
+const GatingToken = ({
+  nftContract,
+}: {
+  nftContract: ContractAddr
+}): ReactElement => {
+  const { name } = useNft({ nftContract })
+  const { data: tokenName = '' } = useReactQuery(
+    [QueryKeyEnum.NFT_TOKEN_NAME, nftContract],
+    name
+  )
+  const { uri } = useNftImage({ nftContract, tokenId: '1' })
+
+  return (
+    <Row style={{ alignItems: 'center', columnGap: 10 }}>
+      <FormImage source={{ uri }} size={40} />
+      <Text style={{ fontSize: 24 }}>{tokenName}</Text>
+    </Row>
+  )
+}
 
 const ChannelTokenGatingScreen = (): ReactElement => {
   const { navigation, params } = useAppNavigation<Routes.ChannelTokenGating>()
-
+  const { user } = useAuth()
   const [editGatingToken, setEditGatingToken] = useState('' as ContractAddr)
   const editGatingTokenErrMsg = useMemo(() => {
     if (editGatingToken) {
@@ -28,6 +68,16 @@ const ChannelTokenGatingScreen = (): ReactElement => {
     channelUrl: params.channelUrl,
   })
 
+  const { nftList } = useUserNftList({ userAddress: user?.address })
+  const groupedNftList = useMemo(
+    () =>
+      _.map(
+        _.groupBy(nftList, x => x.token_address),
+        val => val[0]
+      ),
+    [nftList]
+  )
+
   return (
     <Container style={styles.container}>
       <Header
@@ -38,29 +88,63 @@ const ChannelTokenGatingScreen = (): ReactElement => {
         onPressLeft={navigation.goBack}
       />
       <View style={styles.body}>
-        <View>
+        <View style={{ flex: 1 }}>
           <View
-            style={{ borderBottomWidth: 1, paddingBottom: 5, marginBottom: 5 }}>
-            <Text>Token : {fsChannelField?.gatingToken || '-'}</Text>
+            style={{
+              borderBottomColor: COLOR.gray._400,
+              borderBottomWidth: 1,
+              paddingBottom: 10,
+              marginBottom: 10,
+            }}>
+            <Text>Gating Token</Text>
+            {fsChannelField?.gatingToken ? (
+              <GatingToken nftContract={fsChannelField?.gatingToken} />
+            ) : (
+              <Text>None</Text>
+            )}
           </View>
-          <Text>Update</Text>
-          <FormInput
-            value={editGatingToken}
-            onChangeText={(value): void => {
-              setEditGatingToken(value as ContractAddr)
-            }}
-          />
+          <View style={{ marginBottom: 10 }}>
+            <Text>Selected NFT</Text>
+            <FormInput
+              value={editGatingToken}
+              onChangeText={(value): void => {
+                setEditGatingToken(value as ContractAddr)
+              }}
+            />
+          </View>
           {editGatingTokenErrMsg && (
             <Text style={{ color: COLOR.error }}>{editGatingTokenErrMsg}</Text>
           )}
+          <FlatList
+            data={groupedNftList}
+            keyExtractor={(item, index): string => `groupedNftList-${index}`}
+            numColumns={2}
+            style={{ flex: 1 }}
+            contentContainerStyle={{ gap: 10 }}
+            columnWrapperStyle={{ gap: 10 }}
+            scrollEnabled={true}
+            renderItem={({ item }): ReactElement => (
+              <TouchableOpacity
+                style={{ flex: 1 / 2 }}
+                onPress={(): void => {
+                  setEditGatingToken(item.token_address)
+                }}>
+                <View style={{ borderRadius: 10, flex: 1 }}>
+                  <MoralisNftRenderer item={item} width={'100%'} height={180} />
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
-        <FormButton
-          disabled={isFetching}
-          onPress={(): void => {
-            updateGatingToken(editGatingToken)
-          }}>
-          Confirm
-        </FormButton>
+        <View style={{ paddingTop: 10 }}>
+          <FormButton
+            disabled={isFetching}
+            onPress={(): void => {
+              updateGatingToken(editGatingToken)
+            }}>
+            Confirm
+          </FormButton>
+        </View>
       </View>
     </Container>
   )
