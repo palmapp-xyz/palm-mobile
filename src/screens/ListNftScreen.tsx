4e85ea8fd1ebba90c47d2c85f434c057ce404693
Lim SangHyun <skqksh@gmail.com>
2023-03-05 01:36:02 -0700

    Update listing NFT UI

--- src/screens/ListNftScreen.tsx
+++ src/screens/ListNftScreen.tsx
@@ -6,9 +6,10 @@ import { SignedNftOrderV4Serialized } from '@traderxyz/nft-swap-sdk'
 import firestore from '@react-native-firebase/firestore'
 import { useGroupChannel } from '@sendbird/uikit-chat-hooks'
 import { useSendbirdChat } from '@sendbird/uikit-react-native'
+import Ionicon from 'react-native-vector-icons/Ionicons'
 
 import { COLOR, UTIL } from 'consts'
-import { Moralis, Token } from 'types'
+import { Moralis, QueryKeyEnum, Token } from 'types'
 import {
   Header,
   SubmitButton,
@@ -16,6 +17,7 @@ import {
   Container,
   Row,
   NftRenderer,
+  EthLogoWrapper,
 } from 'components'
 import useZxListNft from 'hooks/zx/useZxListNft'
 import { Routes } from 'libs/navigation'
@@ -24,6 +26,8 @@ import selectNftStore from 'store/selectNftStore'
 import { nftUriFetcher } from 'libs/nft'
 import { stringifySendFileData } from 'libs/sendbird'
 import useFsChannel from 'hooks/firestore/useFsChannel'
+import useNft from 'hooks/contract/useNft'
+import useReactQuery from 'hooks/complex/useReactQuery'
 
 const Contents = ({
   channelUrl,
@@ -32,15 +36,23 @@ const Contents = ({
   channelUrl: string
   selectedNft: Moralis.NftItem
 }): ReactElement => {
+  const nftContract = selectedNft.token_address
   const { price, setPrice, isApproved, onClickApprove, onClickConfirm } =
     useZxListNft({
-      nftContract: selectedNft.token_address,
+      nftContract,
       tokenId: selectedNft.token_id,
     })
   const { sdk } = useSendbirdChat()
   const { channel } = useGroupChannel(sdk, channelUrl)
 
   const { fsChannel } = useFsChannel({ channelUrl })
+
+  const { name } = useNft({ nftContract })
+
+  const { data: tokenName = '' } = useReactQuery(
+    [QueryKeyEnum.NFT_TOKEN_NAME, nftContract],
+    name
+  )
 
   const onSubmit = async (
     token_uri: string,
@@ -79,69 +91,118 @@ const Contents = ({
 
   return (
     <View style={styles.body}>
-      <View>
+      <View style={{ paddingHorizontal: 20 }}>
         <Row style={{ paddingBottom: 10 }}>
           <View style={{ width: 100, height: 100, marginEnd: 10 }}>
-            <NftRenderer
-              nftContract={selectedNft.token_address}
-              tokenId={selectedNft.token_id}
-            />
+            <EthLogoWrapper>
+              <NftRenderer
+                nftContract={selectedNft.token_address}
+                tokenId={selectedNft.token_id}
+              />
+            </EthLogoWrapper>
           </View>
           <View style={{ rowGap: 10 }}>
-            <Text>Address : {UTIL.truncate(selectedNft.token_address)}</Text>
-            <Text>ID : {selectedNft.token_id}</Text>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+              {tokenName}
+            </Text>
+            <Text>#{selectedNft.token_id}</Text>
           </View>
         </Row>
-        <Text style={{ fontSize: 20 }}>Price</Text>
-        <FormInput
-          maxLength={10}
-          value={price}
-          onChangeText={(value): void => {
-            if (!value) {
-              return
-            }
-            setPrice(value as Token)
-          }}
-        />
+        <Row style={styles.traitsBox}>
+          <View style={styles.traits}>
+            <Text>Background</Text>
+            <Text style={{ color: COLOR.primary._400 }}>Blue</Text>
+          </View>
+          <View style={styles.traits}>
+            <Text>Face</Text>
+            <Text style={{ color: COLOR.primary._400 }}>Default</Text>
+          </View>
+          <View style={styles.traits}>
+            <Text>Hair</Text>
+            <Text style={{ color: COLOR.primary._400 }}>Yellow Star</Text>
+          </View>
+        </Row>
       </View>
       <View>
-        <Row style={{ paddingBottom: 10 }}>
-          <Text
-            style={{
-              fontWeight: 'bold',
-              color: isApproved ? COLOR.gray._700 : COLOR.primary._400,
-            }}>
-            1. Approve
-          </Text>
-          <Text>{' -> '}</Text>
-          <Text
-            style={{
-              fontWeight: 'bold',
-              color: isApproved ? COLOR.primary._400 : COLOR.gray._700,
-            }}>
-            2. Listing
-          </Text>
-        </Row>
         {isApproved ? (
-          <SubmitButton
-            disabled={!price}
-            onPress={async (): Promise<void> => {
-              Keyboard.dismiss()
-              const order = await onClickConfirm()
-              onSubmit(selectedNft.token_uri, order)
-            }}>
-            List up to sell
-          </SubmitButton>
+          <View>
+            <View style={styles.priceBox}>
+              <Row style={{ justifyContent: 'space-between' }}>
+                <Text style={{ color: 'white' }}>Current Floor Price</Text>
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>
+                  2.5 ETH
+                </Text>
+              </Row>
+              <Row style={{ justifyContent: 'space-between' }}>
+                <Text style={{ color: 'white' }}>Gas Fee</Text>
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>
+                  0.01 ETH
+                </Text>
+              </Row>
+              <View style={{ position: 'relative' }}>
+                <FormInput
+                  placeholder="Price"
+                  maxLength={10}
+                  value={price}
+                  onChangeText={(value): void => {
+                    if (!value) {
+                      return
+                    }
+                    setPrice(value as Token)
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 16,
+                    position: 'absolute',
+                    right: 20,
+                    top: 15,
+                  }}>
+                  ETH
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{ paddingHorizontal: 25, gap: 5, paddingVertical: 10 }}>
+              <Row style={{ alignItems: 'flex-start' }}>
+                <Ionicon name="checkmark-circle-outline" size={14} />
+                <Text style={{ fontSize: 14, paddingRight: 15 }}>
+                  The sale will be accepted and concluded unless you cancel it.
+                </Text>
+              </Row>
+              <Row style={{ alignItems: 'flex-start' }}>
+                <Ionicon name="checkmark-circle-outline" size={14} />
+                <Text style={{ fontSize: 14 }}>
+                  The sale listing is valid for 24 hours.
+                </Text>
+              </Row>
+            </View>
+            <SubmitButton
+              containerStyle={{ borderRadius: 0 }}
+              disabled={!price}
+              onPress={async (): Promise<void> => {
+                Keyboard.dismiss()
+                const order = await onClickConfirm()
+                onSubmit(selectedNft.token_uri, order)
+              }}>
+              List up to sell
+            </SubmitButton>
+          </View>
         ) : (
           <View>
+            <View style={{ paddingHorizontal: 20, paddingVertical: 10 }}>
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+                Approve to list your NFT
+              </Text>
+            </View>
             <SubmitButton
+              containerStyle={{ borderRadius: 0 }}
               onPress={(): void => {
                 Keyboard.dismiss()
                 onClickApprove()
               }}>
               Approve
             </SubmitButton>
-            <Text>Approve to sell item</Text>
           </View>
         )}
       </View>
@@ -175,8 +236,27 @@ export default ListNftScreen
 
 const styles = StyleSheet.create({
   body: {
-    padding: 20,
+    paddingTop: 20,
     flex: 1,
     justifyContent: 'space-between',
+    backgroundColor: 'white',
+  },
+  priceBox: {
+    backgroundColor: COLOR.primary._400,
+    borderRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    rowGap: 10,
+    marginHorizontal: 20,
+  },
+  traitsBox: {
+    columnGap: 10,
+  },
+  traits: {
+    rowGap: 5,
+    alignItems: 'center',
+    backgroundColor: COLOR.primary._100,
+    borderRadius: 15,
+    padding: 20,
   },
 })
