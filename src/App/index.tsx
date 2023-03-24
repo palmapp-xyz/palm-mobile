6f7dae3d965fbdcbbd1094680e58459ae98a054a
Jong Hyuck Won <jhwon0820@gmail.com>
2023-03-24 14:26:13 +0900

    disabled firestore persistance

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,12 +3,21 @@ import React, { ReactElement } from 'react'
 import AppProvider from './AppProvider'
 import Navigation from '../Navigation'
 import { LogBox } from 'react-native'
+import { useAsyncEffect } from '@sendbird/uikit-utils'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import firestore, {
+  FirebaseFirestoreTypes,
+} from '@react-native-firebase/firestore'
 
 const queryClient = new QueryClient()
 
 const App = (): ReactElement => {
   LogBox.ignoreAllLogs()
+  useAsyncEffect(async () => {
+    const setting: FirebaseFirestoreTypes.Settings = { persistence: false }
+    await firestore().settings(setting)
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <AppProvider>
