b046d0e3c30dcb905e5cd8b5706ac5190e236a7b
blue <skqksh@gmail.com>
2023-05-17 16:12:20 +0900

    Revert channel header

--- src/screens/uikit-app/GroupChannelScreen/GroupChannelHeader.tsx
+++ src/screens/uikit-app/GroupChannelScreen/GroupChannelHeader.tsx
@@ -1,37 +1,77 @@
 import images from 'assets/images'
 import { FormImage, Row } from 'components'
+import useAuth from 'hooks/auth/useAuth'
 import { useAppNavigation } from 'hooks/useAppNavigation'
 import { Routes } from 'libs/navigation'
 import React, { ReactElement, useContext } from 'react'
 import { TouchableOpacity, View } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { SbUserMetadata } from 'types'
 
+import { Member } from '@sendbird/chat/groupChannel'
 import {
   GroupChannelContexts,
   GroupChannelProps,
   useLocalization,
 } from '@sendbird/uikit-react-native'
 import {
-  createStyleSheet,
+  Avatar,
   Header,
+  createStyleSheet,
   useHeaderStyle,
 } from '@sendbird/uikit-react-native-foundation'
 
 const GroupChannelHeader = ({
   onPressHeaderLeft,
 }: GroupChannelProps['Header']): ReactElement => {
+  const { user } = useAuth()
   const { navigation, params } = useAppNavigation<Routes.GroupChannel>()
-  const { headerTitle } = useContext(GroupChannelContexts.Fragment)
+  const { headerTitle, channel } = useContext(GroupChannelContexts.Fragment)
   const { typingUsers } = useContext(GroupChannelContexts.TypingIndicator)
   const { STRINGS } = useLocalization()
   const { HeaderComponent } = useHeaderStyle()
   const subtitle = STRINGS.LABELS.TYPING_INDICATOR_TYPINGS(typingUsers)
+
+  const isMyDM =
+    channel.memberCount === 2 &&
+    channel.members.filter(
+      (member: Member) => member.userId === user?.auth?.profileId
+    ).length === 1
+  const otherDMUser: Member | undefined = isMyDM
+    ? channel.members.filter(
+        (member: Member) => member.userId !== user?.auth?.profileId
+      )[0]
+    : undefined
 
   return (
     <HeaderComponent
       clearTitleMargin
       title={
         <View style={styles.titleContainer}>
+          {otherDMUser ? (
+            <TouchableOpacity
+              onPress={(): void => {
+                navigation.navigate(Routes.UserProfile, {
+                  address: (otherDMUser.metaData as SbUserMetadata).address,
+                  profileId: otherDMUser.userId,
+                })
+              }}
+            >
+              <Avatar
+                size={36}
+                uri={otherDMUser.profileUrl}
+                containerStyle={styles.avatarGroup}
+              />
+            </TouchableOpacity>
+          ) : (
+            channel.coverUrl && (
+              <FormImage
+                source={{ uri: channel.coverUrl }}
+                size={36}
+                style={styles.avatarGroup}
+              />
+            )
+          )}
           <View style={{ flexShrink: 1 }}>
             <Header.Title h2>{headerTitle}</Header.Title>
             {Boolean(subtitle) && subtitle && (
