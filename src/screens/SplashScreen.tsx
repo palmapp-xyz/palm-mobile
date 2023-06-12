e10a0d6a17a454c903a418c7e292c35967e8d789
H. Park <bokuhe@gmail.com>
2023-06-12 18:26:18 +0900

    fix: adjust splash dimension

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -2,7 +2,14 @@ import Lottie from 'lottie-react-native'
 import React, { ReactElement } from 'react'
 
 const SplashScreen = (): ReactElement => {
-  return <Lottie source={require('../assets/splash.json')} autoPlay loop />
+  return (
+    <Lottie
+      source={require('../assets/splash.json')}
+      style={{ width: 208, height: 208, alignSelf: 'center' }}
+      autoPlay
+      loop
+    />
+  )
 }
 
 export default SplashScreen
