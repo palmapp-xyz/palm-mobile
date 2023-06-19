4c8adabbd6256c27449d2e76efb13dd8530d235e
H. Park <bokuhe@Gmail.com>
2023-06-19 18:26:59 +0900

    fix: PIN reset when logout

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -11,7 +11,7 @@ import { getFsProfile } from 'libs/firebase'
 import { recordError } from 'libs/logger'
 import _ from 'lodash'
 import { useState } from 'react'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 import appStore from 'store/appStore'
 import {
   AuthChallengeResult,
@@ -28,6 +28,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import { useConnection, useSendbirdChat } from '@sendbird/uikit-react-native'
 import { useAsyncEffect } from '@sendbird/uikit-utils'
+import { resetNewPin, resetPin } from 'libs/pin'
 import RNRestart from 'react-native-restart'
 
 export type UseAuthReturn = {
@@ -45,6 +46,7 @@ const useAuth = (): UseAuthReturn => {
   const [user, setUser] = useRecoilState(appStore.user)
   const { connect, disconnect } = useConnection()
   const { setCurrentUser } = useSendbirdChat()
+  const setLoading = useSetRecoilState(appStore.loading)
 
   const {
     authenticate: lensAuthenticate,
@@ -223,12 +225,15 @@ const useAuth = (): UseAuthReturn => {
       removeKeys(),
       auth().signOut(),
       disconnect(),
+      resetPin(),
+      resetNewPin(),
     ])
 
     setCurrentUser(undefined)
     setRestoreLoading(false)
     setUser(undefined)
 
+    setLoading(true)
     RNRestart.restart()
   }
 
