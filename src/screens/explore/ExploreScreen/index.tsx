fa29ea8c93cc26b6a90c0ae7212d25767898b1c5
H. Park <bokuhe@Gmail.com>
2023-06-19 18:33:51 +0900

    feat: when pin is not set, set a pin when entering ExploreScreen

--- src/screens/explore/ExploreScreen/index.tsx
+++ src/screens/explore/ExploreScreen/index.tsx
@@ -3,7 +3,7 @@ import { Container, FormImage, FormInput, Row } from 'components'
 import { COLOR } from 'consts'
 import useExploreSearch from 'hooks/page/explore/useExploreSearch'
 //import { useAppNavigation } from 'hooks/useAppNavigation'
-import React, { ReactElement, useRef, useState } from 'react'
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
 import {
   Animated,
   ScrollView,
@@ -14,6 +14,9 @@ import {
 } from 'react-native'
 import Ionicon from 'react-native-vector-icons/Ionicons'
 
+import { useAppNavigation } from 'hooks/useAppNavigation'
+import { Routes } from 'libs/navigation'
+import { getPin } from 'libs/pin'
 import { useTranslation } from 'react-i18next'
 import RecentlySearched from './RecentlySearched'
 import RecommendChat from './RecommendChat'
@@ -36,6 +39,7 @@ const HEADER_HEIGHT = 72
 // }
 
 const ExploreScreen = (): ReactElement => {
+  const { navigation } = useAppNavigation<Routes.Explore>()
   const { t } = useTranslation()
   const [searchFocused, setSearchFocused] = useState(false)
   const inputRef = useRef<TextInput>(null)
@@ -48,6 +52,23 @@ const ExploreScreen = (): ReactElement => {
     isSearching,
     onClickConfirm,
   } = useExploreSearchReturn
+
+  useEffect(() => {
+    const checkPin = async (): Promise<void> => {
+      const isNotConfigurePin = (await getPin()) === ''
+
+      if (isNotConfigurePin) {
+        navigation.push(Routes.Pin, {
+          type: 'set',
+          result: async (result: boolean): Promise<void> => {
+            result && navigation.pop()
+            return Promise.resolve()
+          },
+        })
+      }
+    }
+    checkPin()
+  }, [])
 
   return (
     <Container style={styles.container}>
