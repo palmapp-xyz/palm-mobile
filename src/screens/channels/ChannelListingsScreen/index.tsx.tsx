ad5f4eefb652469444fd622fa439fd4916e03f7b
H. Park <bokuhe@Gmail.com>
2023-06-23 17:48:22 +0900

    wip: no nft list design

--- src/screens/channels/ChannelListingsScreen/index.tsx.tsx
+++ src/screens/channels/ChannelListingsScreen/index.tsx.tsx
@@ -1,22 +1,32 @@
-import { Container, FormText, Header } from 'components'
+import { Container, FormButton, FormText, Header } from 'components'
 import { COLOR } from 'consts'
 import useFsChannel from 'hooks/firestore/useFsChannel'
 import { useAppNavigation } from 'hooks/useAppNavigation'
 import { recordError } from 'libs/logger'
 import { Routes } from 'libs/navigation'
 import React, { ReactElement, useState } from 'react'
-import { FlatList, RefreshControl, StyleSheet, View } from 'react-native'
+import {
+  FlatList,
+  RefreshControl,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native'
 import { FbListing } from 'types'
 
 import { useAsyncEffect } from '@sendbird/uikit-utils'
 
+import { useTranslation } from 'react-i18next'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import FbListingItem from './FbListingItem'
 
 const Contents = ({ channelUrl }: { channelUrl: string }): ReactElement => {
   const [isFetching, setIsFetching] = useState<boolean>(true)
   const [channelListings, setChannelListings] = useState<FbListing[]>([])
 
+  const { navigation } = useAppNavigation<Routes.ChannelListings>()
   const { fsChannel } = useFsChannel({ channelUrl })
+  const { t } = useTranslation()
 
   useAsyncEffect(async () => {
     if (!fsChannel) {
@@ -46,11 +56,33 @@ const Contents = ({ channelUrl }: { channelUrl: string }): ReactElement => {
 
   if (!fsChannel || (!isFetching && channelListings.length === 0)) {
     return (
-      <View style={styles.empty}>
-        <FormText font={'SB'} color={COLOR.black._300}>
-          No listings found
+      <SafeAreaView style={styles.empty}>
+        <Ionicons
+          name="alert-circle-outline"
+          size={44}
+          color={COLOR.black._300}
+          style={{ margin: 20 }}
+        />
+        <FormText
+          font={'B'}
+          size={18}
+          color={COLOR.black._900}
+          style={{ textAlign: 'center' }}
+        >
+          {t('Nft.ChannelListingNoListedNft')}
         </FormText>
-      </View>
+        <FormButton
+          figure="outline"
+          containerStyle={styles.buttonListNft}
+          textStyle={{ color: COLOR.black._300 }}
+          rightIcon="ios-chevron-forward"
+          onPress={(): void => {
+            // navigation.navigate(Routes.ListNft, { channelUrl })
+          }}
+        >
+          {t('Nft.ChannelListingListNFT')}
+        </FormButton>
+      </SafeAreaView>
     )
   }
 
@@ -110,5 +142,18 @@ export default ChannelListingsScreen
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: 'transparent' },
   body: { flex: 1 },
-  empty: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  empty: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonListNft: {
+    flexDirection: 'row',
+    borderColor: COLOR.black._100,
+    borderRadius: 12,
+    margin: 36,
+    paddingLeft: 20,
+    paddingRight: 14,
+    paddingVertical: 4,
+  },
 })
