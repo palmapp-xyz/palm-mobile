217780cfff134e568f8e56caa373344ad92c9d85
H. Park <bokuhe@gmail.com>
2023-07-23 23:27:21 +0900

    design: operators ui

--- src/palm-react-native-ui-kit/screens/channels/uikit-app/GroupChannelOperatorsScreen.tsx
+++ src/palm-react-native-ui-kit/screens/channels/uikit-app/GroupChannelOperatorsScreen.tsx
@@ -1,36 +1,214 @@
 import { Routes } from 'palm-core/libs/navigation'
 import { useAppNavigation } from 'palm-react-native/app/useAppNavigation'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
 
+import { ProfileMedia } from '@lens-protocol/react'
+import { Member } from '@sendbird/chat/groupChannel'
 import { useGroupChannel } from '@sendbird/uikit-chat-hooks'
+import { useSendbirdChat } from '@sendbird/uikit-react-native'
+import { COLOR } from 'palm-core/consts'
+import { getProfileMediaImg } from 'palm-core/libs/lens'
 import {
-  createGroupChannelOperatorsFragment,
-  useSendbirdChat,
-} from '@sendbird/uikit-react-native'
+  Container,
+  FormButton,
+  FormText,
+  Header,
+  Row,
+} from 'palm-react-native-ui-kit/components'
+import Avatar from 'palm-react-native-ui-kit/components/sendbird/Avatar'
+import useToast from 'palm-react-native/app/useToast'
+import { useTranslation } from 'react-i18next'
+import {
+  Animated,
+  FlatList,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native'
+import { RectButton, Swipeable } from 'react-native-gesture-handler'
 
-const GroupChannelOperatorsFragment = createGroupChannelOperatorsFragment()
+const OperatorUserCard = React.memo(
+  ({
+    handle,
+    picture,
+    onPress,
+  }: {
+    handle?: string
+    handleHighlight?: string
+    picture?: ProfileMedia | string
+    selected?: boolean
+    onPress: () => void
+  }): ReactElement => {
+    const profileImg =
+      typeof picture === 'string' ? picture : getProfileMediaImg(picture)
+
+    return (
+      <TouchableWithoutFeedback onPress={onPress}>
+        <Row style={operatorUserCardStyles.row}>
+          <Avatar uri={profileImg} size={40} />
+          <View style={{ flex: 1 }}>
+            <FormText numberOfLines={1} ellipsizeMode="tail">
+              {handle}
+            </FormText>
+          </View>
+        </Row>
+      </TouchableWithoutFeedback>
+    )
+  }
+)
+const operatorUserCardStyles = StyleSheet.create({
+  row: {
+    height: 40,
+    gap: 12,
+    alignItems: 'center',
+    marginBottom: 16,
+    // paddingHorizontal: 20,
+  },
+})
+
+const Title = ({ title }: { title: string }): ReactElement => {
+  return (
+    <View>
+      <FormText
+        style={{ marginVertical: 12, marginHorizontal: 20 }}
+        color={COLOR.black._300}
+      >
+        {title}
+      </FormText>
+      <View style={{ height: 1, backgroundColor: COLOR.black._90005 }} />
+    </View>
+  )
+}
+
 const GroupChannelOperatorsScreen = (): ReactElement => {
   const { navigation, params } =
     useAppNavigation<Routes.GroupChannelOperators>()
 
   const { sdk } = useSendbirdChat()
   const { channel } = useGroupChannel(sdk, params.channelUrl)
+  const toast = useToast()
+  const { t } = useTranslation()
+
+  const [operators, setOperators] = useState<Member[]>([])
+
+  const rowRefs = useRef<{ [userId: string]: Swipeable | null }[]>([])
+  const [currentRow, setCurrentRow] = useState<Member>()
+
+  useEffect(() => {
+    setOperators(
+      channel?.members.filter(member => member.role === 'operator') ?? []
+    )
+  }, [channel?.members])
+
   if (!channel) {
     return <></>
   }
 
+  const renderRightActions = (progress): ReactElement => {
+    const trans = progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [128, 0],
+      extrapolate: 'clamp',
+    })
+
+    const removeOperator = (): void => {
+      currentRow &&
+        channel
+          .removeOperators([currentRow.userId])
+          .then(() => {
+            toast.show(t('Channels.ChannelOperatorsRemoveSuccessToast'), {
+              icon: 'check',
+              color: 'green',
+            })
+          })
+          .catch(_ => {
+            toast.show(t('Channels.ChannelOperatorsRemoveFailedToast'), {
+              icon: 'info',
+              color: 'red',
+            })
+          })
+    }
+
+    return (
+      <View style={{ width: 80, flexDirection: 'row' }}>
+        <Animated.View style={{ flex: 1, transform: [{ translateX: trans }] }}>
+          <RectButton
+            style={[
+              {
+                backgroundColor: 'red',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: 80,
+                height: '100%',
+              },
+            ]}
+            onPress={removeOperator}
+          >
+            <FormText color={COLOR.white}>{'Remove'}</FormText>
+          </RectButton>
+        </Animated.View>
+      </View>
+    )
+  }
+
+  const onSiwpeableWillOpen = (item: Member): void => {
+    Object.keys(rowRefs.current).forEach(id => {
+      if (item.userId !== id) {
+        rowRefs.current[id]?.close()
+      }
+    })
+  }
+
   return (
-    <GroupChannelOperatorsFragment
-      channel={channel}
-      onPressHeaderLeft={(): void => {
-        // Navigate back
-        navigation.goBack()
-      }}
-      onPressHeaderRight={(): void => {
-        // Navigate to group channel set as operators
-        navigation.navigate(Routes.GroupChannelRegisterOperator, params)
-      }}
-    />
+    <Container style={{ flex: 1 }}>
+      <Header left={'back'} onPressLeft={(): void => navigation.goBack()} />
+      <Title title={t('Channels.ChannelOperatorsTitle')} />
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={operators}
+          renderItem={({ item }): ReactElement => {
+            return (
+              <Swipeable
+                ref={(ref): void => {
+                  rowRefs.current[item.userId] = ref
+                }}
+                containerStyle={{ marginTop: 16, paddingHorizontal: 20 }}
+                renderRightActions={(progress): ReactElement | null => {
+                  if (sdk.currentUser.userId === item.userId) {
+                    return null
+                  } else {
+                    return renderRightActions(progress)
+                  }
+                }}
+                onSwipeableWillOpen={(): void => {
+                  onSiwpeableWillOpen(item)
+                }}
+                onSwipeableOpen={(): void => {
+                  setCurrentRow(item)
+                }}
+                childrenContainerStyle={{ flex: 1 }}
+              >
+                <OperatorUserCard
+                  handle={item.nickname}
+                  picture={item.profileUrl}
+                  onPress={(): void => {}}
+                />
+              </Swipeable>
+            )
+          }}
+        />
+        <FormButton
+          font="SB"
+          figure="outline"
+          onPress={(): void => {
+            // todo - select view
+          }}
+          containerStyle={{ marginHorizontal: 20 }}
+        >
+          {t('Channels.ChannelOperatorsAddOperators')}
+        </FormButton>
+      </View>
+    </Container>
   )
 }
 
