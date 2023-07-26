02a1db83e3c4480adf58583816853a7b5020568a
H. Park <bokuhe@gmail.com>
2023-07-27 01:55:43 +0900

    feat: sentry integration

--- src/palm-react-native-ui-kit/App/index.tsx
+++ src/palm-react-native-ui-kit/App/index.tsx
@@ -1,11 +1,18 @@
-import Loading from 'palm-react-native-ui-kit/components/atoms/Loading'
+import * as Sentry from '@sentry/react-native'
 import Navigation from 'palm-react-native-ui-kit/Navigation'
+import Loading from 'palm-react-native-ui-kit/components/atoms/Loading'
 import UpdateScreen from 'palm-react-native-ui-kit/screens/app/UpdateScreen'
 import useCodePush from 'palm-react-native/app/useCodePush'
+import Config from 'palm-react-native/config'
 import React, { ReactElement } from 'react'
 import { Keyboard, LogBox, TouchableWithoutFeedback } from 'react-native'
-
 import AppProviderWrapper from './AppProvider'
+
+!__DEV__ &&
+  Sentry.init({
+    dsn: Config.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  })
 
 const App = (): ReactElement => {
   LogBox.ignoreAllLogs()
@@ -28,4 +35,4 @@ const App = (): ReactElement => {
   )
 }
 
-export default App
+export default Sentry.wrap(App)
