684e5470546b607dbe73ba8e1549840300fa365d
Jong Hyuck Won <denniswon@users.noreply.github.com>
2023-07-28 01:15:03 +0900

    Revert "feat: sentry integration"

--- src/palm-react-native-ui-kit/App/index.tsx
+++ src/palm-react-native-ui-kit/App/index.tsx
@@ -1,18 +1,11 @@
-import * as Sentry from '@sentry/react-native'
-import Navigation from 'palm-react-native-ui-kit/Navigation'
 import Loading from 'palm-react-native-ui-kit/components/atoms/Loading'
+import Navigation from 'palm-react-native-ui-kit/Navigation'
 import UpdateScreen from 'palm-react-native-ui-kit/screens/app/UpdateScreen'
 import useCodePush from 'palm-react-native/app/useCodePush'
-import Config from 'palm-react-native/config'
 import React, { ReactElement } from 'react'
 import { Keyboard, LogBox, TouchableWithoutFeedback } from 'react-native'
-import AppProviderWrapper from './AppProvider'
 
-!__DEV__ &&
-  Sentry.init({
-    dsn: Config.SENTRY_DSN,
-    tracesSampleRate: 1.0,
-  })
+import AppProviderWrapper from './AppProvider'
 
 const App = (): ReactElement => {
   LogBox.ignoreAllLogs()
@@ -35,4 +28,4 @@ const App = (): ReactElement => {
   )
 }
 
-export default Sentry.wrap(App)
+export default App
