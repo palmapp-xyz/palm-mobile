42be3052534ff7217beca99c4633f87d63b38d2d
H. Park <bokuhe@gmail.com>
2023-07-28 14:17:11 +0900

    fix: adjusted splash image position for android

--- src/palm-react-native-ui-kit/screens/app/SplashScreen.tsx
+++ src/palm-react-native-ui-kit/screens/app/SplashScreen.tsx
@@ -1,10 +1,12 @@
 import Lottie from 'lottie-react-native'
 import images from 'palm-ui-kit/assets/images'
 import React, { ReactElement } from 'react'
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
 import ExtraDimensions from 'react-native-extra-dimensions-android'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const SplashScreen = (): ReactElement => {
+  const insets = useSafeAreaInsets()
   const softMenuBarHeight = ExtraDimensions.getSoftMenuBarHeight()
 
   return (
@@ -12,7 +14,9 @@ const SplashScreen = (): ReactElement => {
       style={{
         flex: 1,
         justifyContent: 'center',
-        marginBottom: -softMenuBarHeight,
+        marginTop: Platform.select({
+          android: softMenuBarHeight === 0 ? insets.top : softMenuBarHeight,
+        }),
       }}
     >
       <Lottie
