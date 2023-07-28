709bc5f6acf68ada4d9fe47ef282b19e845e0771
H. Park <bokuhe@gmail.com>
2023-07-28 12:50:58 +0900

    design: update splash animations

--- src/palm-react-native-ui-kit/screens/app/SplashScreen.tsx
+++ src/palm-react-native-ui-kit/screens/app/SplashScreen.tsx
@@ -1,15 +1,27 @@
 import Lottie from 'lottie-react-native'
 import images from 'palm-ui-kit/assets/images'
 import React, { ReactElement } from 'react'
+import { View } from 'react-native'
+import ExtraDimensions from 'react-native-extra-dimensions-android'
 
 const SplashScreen = (): ReactElement => {
+  const softMenuBarHeight = ExtraDimensions.getSoftMenuBarHeight()
+
   return (
-    <Lottie
-      source={images.splash}
-      style={{ width: 208, height: 208, alignSelf: 'center' }}
-      autoPlay
-      loop
-    />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: -softMenuBarHeight,
+      }}
+    >
+      <Lottie
+        source={images.splash}
+        style={{ width: 208, height: 208, alignSelf: 'center' }}
+        autoPlay
+        loop
+      />
+    </View>
   )
 }
 
