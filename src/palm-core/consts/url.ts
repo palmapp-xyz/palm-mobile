4a859be275d78c4856e448ad081cc141ddfaebc0
H. Park <bokuhe@gmail.com>
2023-08-08 00:19:48 +0900

    fix: add privacy url

--- src/palm-core/consts/url.ts
+++ src/palm-core/consts/url.ts
@@ -1,2 +1,3 @@
 export const PALM_HOMEPAGE_URL = 'https://palmapp.xyz'
 export const PALM_TERMS_OF_SERVICE_URL = 'https://palmapp.xyz/legal/tos'
+export const PALM_PRIVACY_POLICY_URL = 'https://palmapp.xyz/legal/privacy'
